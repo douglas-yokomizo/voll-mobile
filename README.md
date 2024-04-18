1caea537ac53e350aad31dcc49e1fe646664d537
Douglas <yogi.yokomizo@gmail.com>
2024-04-18 15:57:59 -0300

    refactor: signup component

---
+++ README.md
@@ -0,0 +1,27 @@
+# Voll
+An application for medical consultations using Native Base and TypeScript in React Native.
+
+## 🔨 Application Features
+The application allows users to schedule appointments with doctors, view their medical histories, and search for specialists in the region.
+
+## ✔️ Technologies and Tools Used
+The technologies and tools used in this application include:
+
+- Native Base: a visual component library for React Native
+- TypeScript: a strongly typed programming language
+- Expo: an application development platform for React Native
+- React Native: a framework for developing applications for mobile platforms
+
+## 🎯 Challenge
+Use the Native Base library and TypeScript to create a medical consultation application.
+
+## 📁 Access to the Project
+The source code for this application can be accessed in its GitHub repository.
+
+## 🛠️ Execution Instructions
+1. Clone the repository to your local machine
+2. Install the necessary dependencies with the npm install command
+3. Run the application with the npx expo start command
+
+## 📚 More Information
+For more information about this project, please contact the developer.
--- src/Signup.tsx
+++ src/Signup.tsx
@@ -1,67 +1,48 @@
-import {VStack, Image, Box, Button,} from 'native-base'
+import {Text, Image, Box, Checkbox, ScrollView} from 'native-base'
 import Logo from './assets/Logo.png'
 import { Title } from './components/Title';
 import { TextInput } from './components/TextInput';
 import { useState } from 'react';
 import { Botao } from './components/Botao';
+import { sections } from '../src/utils/SignupInputs';
 
 export default function Login() {
   const [sectionNumber, setSectionNumber] = useState(0)
-  const sections = [
-    {
-      id: 1,
-      title: 'Insira alguns dados básicos',
-      textInput: [
-        {
-          id: 1,
-          label: 'Nome',
-          placeholder: 'Digite seu nome completo'
-        },
-        {
-          id: 2,
-          label: 'Email',
-          placeholder: 'Digite seu email'
-        },
-      ]
-    },
-    {
-      id: 2,
-      title: 'Agora mais alguns dados sobre você',
-      textInput: [
-        {
-          id: 1,
-          label: 'CEP',
-          placeholder: 'Digite seu CEP'
-        }
-      ]
-    }
-  ]
 
   const handleNextSection = () => {
     if (sectionNumber < sections.length - 1) {
       setSectionNumber(sectionNumber + 1)
-    } else {
-
     }
   }
 
   const handlePrevSection = () => {
-    if (sectionNumber > 0 && sectionNumber === sections.length - 1) {
+    if (sectionNumber > 0) {
       setSectionNumber(sectionNumber - 1)
-    } else {
-
     }
   }
 
   return (
-    <VStack flex={1} alignItems="center" justifyContent="center" p={5}>
-      <Image source={Logo} alt='Logo Voll'/>
+    <ScrollView flex={1} p={5} my={12}>
+      <Image source={Logo} alt='Logo Voll' alignSelf={'center'}/>
       <Title>
         {sections[sectionNumber].title}
       </Title>
       <Box>
-        {sections[sectionNumber].textInput.map((input) => {
+        {sections[sectionNumber]?.textInput?.map((input) => {
           return <TextInput key={input.id} label={input.label} placeholder={input.placeholder}/>
+        })}
+      </Box>
+      {
+        sectionNumber === 2 &&
+      <Text color={'blue.800'} fontSize={'md'} my={2} fontWeight={'bold'}>Selecione os planos:</Text>
+      }
+      <Box>
+        {sections[sectionNumber].checkbox.map((checkbox) => {
+          return (
+          <Checkbox key={checkbox.id} value={checkbox.value}>
+            {checkbox.value}
+          </Checkbox>
+          )
         })}
       </Box>
       {
@@ -70,6 +51,6 @@ export default function Login() {
       }
         <Botao onPress={() => handleNextSection()} mt={4} borderRadius={'lg'} bgColor={'blue.800'}>Avançar</Botao>
 
-    </VStack>
+    </ScrollView>
   );
 }
