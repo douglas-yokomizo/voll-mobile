4cf40a9163392dddd041774a6bb7f8d87f5d66cc
Douglas Yokomizo <yogi.yokomizo@gmail.com>
2024-05-01 13:34:30 -0300

    feat: update profile component with additiontal information and styling

--- src/Tabs/Profile.tsx
+++ src/Tabs/Profile.tsx
@@ -1,9 +1,37 @@
-import { VStack, Text } from "native-base";
+import { VStack, Text, ScrollView, Avatar, Image, Divider } from "native-base";
+import { Title } from "../components/Title";
 
 export default function Profile() {
   return (
-    <VStack>
-      <Text>anda</Text>
-    </VStack>
+    <ScrollView>
+      <VStack alignItems={"center"}>
+        <Title mt={"16"} color={"blue.500"}>
+          Meu Perfil
+        </Title>
+        <Avatar
+          source={{ uri: "https://www.github.com/douglas-yokomizo.png" }}
+          h={20}
+          w={20}
+        />
+        <Title fontSize={"xl"} mt={16} color={"blue.500"} mb={0}>
+          Informações pessoais
+        </Title>
+        <Title fontSize={"lg"} mt={6} mb={0}>
+          Douglas Yokomizo
+        </Title>
+        <Title fontSize={"md"} mt={2} mb={0}>
+          07/08/1995
+        </Title>
+        <Title fontSize={"md"} mt={2}>
+          São Paulo
+        </Title>
+        <Divider mt={6} width={"90%"} height={0.5} />
+        <Title fontSize={"xl"} mt={10} color={"blue.500"} mb={0}>
+          Histórico médico
+        </Title>
+        <Text>Bronquite</Text>
+        <Text>Sinusite</Text>
+      </VStack>
+    </ScrollView>
   );
 }
