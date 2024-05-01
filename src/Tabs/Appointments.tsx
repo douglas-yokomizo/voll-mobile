58617f787c702e67d2eaeae5cecaa212f887138f
Douglas Yokomizo <yogi.yokomizo@gmail.com>
2024-05-01 17:30:55 -0300

    feat: add `CardAppointments` component to Appointments tab

--- src/Tabs/Appointments.tsx
+++ src/Tabs/Appointments.tsx
@@ -1,9 +1,60 @@
-import { VStack, Text } from "native-base"
+import { VStack, ScrollView, Text, Divider } from "native-base";
+import { Title } from "../components/Title";
+import { Botao } from "../components/Botao";
+import CardAppointments from "../components/CardAppointments";
 
 export default function Appointments() {
   return (
-    <VStack>
-        <Text>Appointments</Text>
-    </VStack>
-  )
+    <ScrollView>
+      <VStack alignItems={"center"}>
+        <Title color={"blue.500"} mt={12} mb={4}>
+          Minhas consultas
+        </Title>
+        <Botao w={"90%"} p={4}>
+          <Text fontWeight={"bold"} fontFamily={"xl"} color={"white"}>
+            Agendar outra consulta
+          </Text>
+        </Botao>
+      </VStack>
+      <VStack alignItems={"start"} ml={6}>
+        <Title color={"blue.500"} fontSize={"lg"}>
+          Proxímas consultas
+        </Title>
+        <CardAppointments
+          name="Dr. Douglas"
+          date="24/05/2024"
+          picture="https://github.com/douglas-yokomizo.png"
+          specialty="Cardiologista"
+          hasAttended={false}
+        />
+      </VStack>
+      <Divider mt={6} borderWidth={0.5} w={"90%"} alignSelf={"center"} />
+      <VStack alignItems={"start"} ml={6}>
+        <Title color={"blue.500"} fontSize={"lg"}>
+          Consultas passadas
+        </Title>
+        <CardAppointments
+          name="Dr. Douglas"
+          date="14/03/2024"
+          picture="https://github.com/douglas-yokomizo.png"
+          specialty="Cardiologista"
+          hasAttended={true}
+        />
+        <CardAppointments
+          name="Dr. Douglas"
+          date="14/03/2024"
+          picture="https://github.com/douglas-yokomizo.png"
+          specialty="Cardiologista"
+          hasAttended={true}
+        />
+        <CardAppointments
+          name="Dr. Douglas"
+          date="14/03/2024"
+          picture="https://github.com/douglas-yokomizo.png"
+          specialty="Cardiologista"
+          hasAttended={true}
+        />
+      </VStack>
+    </ScrollView>
+  );
 }
---
+++ src/components/CardAppointments.tsx
@@ -0,0 +1,41 @@
+import { Avatar, Text, VStack } from "native-base";
+import { Botao } from "./Botao";
+
+interface CardAppointmentsProps {
+  name: string;
+  specialty: string;
+  picture: string;
+  date: string;
+  hasAttended?: boolean;
+}
+
+export default function CardAppointments({
+  name,
+  specialty,
+  date,
+  picture,
+  hasAttended,
+}: CardAppointmentsProps) {
+  return (
+    <VStack
+      mb={4}
+      w={"90%"}
+      shadow={1}
+      borderColor={"gray.200"}
+      borderWidth={1}
+      p={4}
+      borderRadius={"lg"}
+      bgColor={hasAttended ? "blue.200" : "white"}
+    >
+      <VStack flexDir={"row"} alignItems={"center"}>
+        <Avatar size={"lg"} source={{ uri: picture }} />
+        <VStack ml={4}>
+          <Text fontSize={"lg"}>{name}</Text>
+          <Text>{specialty}</Text>
+          <Text>{date}</Text>
+        </VStack>
+      </VStack>
+      {hasAttended ? <Botao>Agendar Consulta</Botao> : <Botao>Cancelar</Botao>}
+    </VStack>
+  );
+}
